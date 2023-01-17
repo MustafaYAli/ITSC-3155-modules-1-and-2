4b7a7fb56b032acf32e4cb4b93370cd129e373f4
MustafaYAli <96496560+MustafaYAli@users.noreply.github.com>
2023-01-16 22:03:04 -0500

    ex1

---
+++ exx1.py
@@ -0,0 +1,19 @@
+
+grade = int(input("Enter your grade: "))
+# Determine the letter grade
+if grade >= 90:
+    print("The letter grade is A.")
+elif grade >= 80:
+    print("The letter grade is B.")
+elif grade >= 70:
+    print("The letter grade is C.")
+elif grade >= 60:
+    print("The letter grade is D.")
+elif grade < 60:
+    print("The letter grade is F.")
+else:
+    print("Invalid grade.")
+
+
+
+
